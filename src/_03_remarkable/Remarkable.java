982526c62944393ed452219749048bcbf3094ec6
League Student <league@iMac-21.attlocal.net>
2019-12-04 16:56:25 -0800

    today id completed 1 thing!!!
    fun, and interesting

--- src/_03_remarkable/Remarkable.java
+++ src/_03_remarkable/Remarkable.java
@@ -1,18 +1,18 @@
 package _03_remarkable;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
+
 //    Level 0
 
 import javax.swing.JOptionPane;
 
 public class Remarkable {
-public static void main(String[] args) {
-String name = JOptionPane.showInputDialog(null ,"What is your name?");
+	public static void main(String[] args) {
+		String name = JOptionPane.showInputDialog(null, "What is your name?");
 		// 1. Save something remarkable about each person in a variable.
-String remarkable = JOptionPane.showInputDialog(null ,"what is remarkable about you?");
+		String remarkable = JOptionPane.showInputDialog(null, "what is remarkable about you?");
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-JOptionPane.showMessageDialog();
-		// 3. In a pop-up, tell the user what is remarkable about that person. 
+		JOptionPane.showMessageDialog(null, remarkable + " is an awesome skill, " + name);
+		// 3. In a pop-up, tell the user what is remarkable about that person.
 
 	}
 }
-
--- src/_05_tasty_tomato/tasty_tomato/tasty_tomato.pde
+++ src/_05_tasty_tomato/tasty_tomato/tasty_tomato.pde
@@ -4,7 +4,10 @@ void setup() {
 void draw() {
     background(200, 200, 200);
     noStroke();
+    fill(200,100,50);
     ellipse(150, 200, 150, 150);
     ellipse(212, 200, 150, 150);
+    fill(100,200,0);
     rect(176, 103, 12, 32);
+rect(100,100,100,100);
 }
