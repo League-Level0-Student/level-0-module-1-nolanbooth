98994a4ec7ad213ec401513d346465192124dc08
League Student <league@iMac-21.attlocal.net>
2020-01-01 16:58:29 -0800

    I finished the riddler.
    it was fun! I liked figuring the things out

--- src/_07_riddler/TheRiddler.java
+++ src/_07_riddler/TheRiddler.java
@@ -2,26 +2,37 @@ package _07_riddler;
 
 import javax.swing.JOptionPane;
 
-//    Copyright (c) The League of Amazing Programmers 2013-2019
-//    Level 0
-
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
-		// 1. Make a variable to hold the score
 		int score = 0;
-		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-		JOptionPane.showInputDialog(null,
-				"When you add two letters to a five letter word, it becomes shorter. how can this be.");
-		// 4. If they got the answer right, pop up "correct!" and increase the score by
-		// one
 
-		// 5. Otherwise, say "wrong" and tell them the answer
+		String answer = JOptionPane.showInputDialog(null,
+				"When you add two letters to a five letter word, it becomes shorter. how can this be? (Type in the original five letter word)");
 
-		// 6. Add some more riddles
+		if (answer.equalsIgnoreCase("Short")) {
+			JOptionPane.showMessageDialog(null, "Correct, good job!");
+			score++;
+		} else {
+			JOptionPane.showMessageDialog(null, " WRONG! The five letter word is 'Short'!");
+		}
+		String yesNoAnswer = JOptionPane.showInputDialog(null, "Do you want to play again?");
+		if (yesNoAnswer.equalsIgnoreCase("Yes")) {
+			String answerTwo = JOptionPane.showInputDialog(null,
+					"What belongs to you but other people use it more often than you?");
+			if (answerTwo.equalsIgnoreCase("your name")) {
+				score++;
+				JOptionPane.showMessageDialog(null, "Correct! Game over (Check your score)!");
 
-		// 2. Make a pop up to show the score.
+			} else {
+				JOptionPane.showMessageDialog(null, "WRONG! the answer is your name. Game over (Check your score)!");
 
+			}
+
+		} else {
+			JOptionPane.showMessageDialog(null, "See ya later (Check your score)!");
+		}
+		System.out.println(score);
 	}
 }
